9974ab26b9667ef27972b104277e619c272ada9c
Charles Ellis <hamled@hamled.name>
2018-09-11 11:12:51 -0700

    Seed data for brushes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+brushes = {
+  FANBLND3: '#3 Fan Blender',
+  FANBLND6: '#6 Fan Blender',
+  RNDHALF: 'Half Size Round',
+  RND1FLG: '1" Round Foliage Brush',
+  KNFPNT10: '#10 Painting Knife',
+  KNFDTL5: '#5 Detail Knife',
+  BLENDER2: '2" Blender',
+  SCPTLIN2: '#2 Script Liner',
+  FILBERT6: '#6 Filbert',
+  BKGRND2: '2" Background',
+  OVLBRST1: '1" Oval Bristle',
+  LNDSCP1: '1" Landscape',
+}
+
+brushes.each do |sku, name|
+  Brush.create!(sku: sku, name: name)
+end
